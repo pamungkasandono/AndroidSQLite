08966c2fb58a113f7f521b5957e5e6458848ab0e
pamungkas andono <pamungkas.org@gmail.com>
2021-04-10 22:03:35 +0700

    kembaliin kaya di awal

--- app/src/main/java/com/example/android2mynotesapp/db/NoteHelper.kt
+++ app/src/main/java/com/example/android2mynotesapp/db/NoteHelper.kt
@@ -9,14 +9,12 @@ import com.example.android2mynotesapp.db.DatabaseContract.NoteColumns.Companion.
 import java.sql.SQLException
 
 class NoteHelper(context: Context) {
-
     private var dataBaseHelper: DatabaseHelper = DatabaseHelper(context)
     private lateinit var database: SQLiteDatabase
 
     companion object {
         private const val DATABASE_TABLE = TABLE_NAME
         private var INSTANCE: NoteHelper? = null
-
         fun getInstance(context: Context): NoteHelper =
             INSTANCE ?: synchronized(this) {
                 INSTANCE ?: NoteHelper(context)
@@ -35,6 +33,7 @@ class NoteHelper(context: Context) {
             database.close()
     }
 
+    // select * from note
     fun queryAll(): Cursor {
         return database.query(
             DATABASE_TABLE,
@@ -47,85 +46,31 @@ class NoteHelper(context: Context) {
             null)
     }
 
+    // select * from note where id
     fun queryById(id: String): Cursor {
-        return database.query(DATABASE_TABLE, null, "$_ID = ?", arrayOf(id), null, null, null, null)
+        return database.query(
+            DATABASE_TABLE,
+            null,
+            "$_ID = ?",
+            arrayOf(id),
+            null,
+            null,
+            null
+        )
     }
 
+    // insert into note
     fun insert(values: ContentValues?): Long {
         return database.insert(DATABASE_TABLE, null, values)
     }
 
-    fun update(id: String, values: ContentValues?): Int {
+    // update note set .. where id
+    fun update(id: String, values: ContentValues): Int {
         return database.update(DATABASE_TABLE, values, "$_ID = ?", arrayOf(id))
     }
+
+    // delete from note where id
     fun delete(id: String): Int {
         return database.delete(DATABASE_TABLE, "$_ID = '$id'", null)
     }
 }
-
-//class NoteHelper(context: Context) {
-//    private var dataBaseHelper: DatabaseHelper = DatabaseHelper(context)
-//    private lateinit var database: SQLiteDatabase
-//
-//    companion object {
-//        private const val DATABASE_TABLE = TABLE_NAME
-//        private var INSTANCE: NoteHelper? = null
-//        fun getInstance(context: Context): NoteHelper =
-//            INSTANCE ?: synchronized(this) {
-//                INSTANCE ?: NoteHelper(context)
-//            }
-//    }
-//
-//    @Throws(SQLException::class)
-//    fun open() {
-//        database = dataBaseHelper.writableDatabase
-//    }
-//
-//    fun close() {
-//        dataBaseHelper.close()
-//
-//        if (database.isOpen)
-//            database.close()
-//    }
-//
-//    // select * from note
-//    fun queryAll(): Cursor {
-//        return database.query(
-//            DATABASE_TABLE,
-//            null,
-//            null,
-//            null,
-//            null,
-//            null,
-//            "$_ID ASC",
-//            null)
-//    }
-//
-//    // select * from note where id
-//    fun queryById(id: String): Cursor {
-//        return database.query(
-//            DATABASE_TABLE,
-//            null,
-//            "$_ID = ?",
-//            arrayOf(id),
-//            null,
-//            null,
-//            null
-//        )
-//    }
-//
-//    // insert into note
-//    fun insert(values: ContentValues?): Long {
-//        return database.insert(DATABASE_TABLE, null, values)
-//    }
-//
-//    // update note set .. where id
-//    fun update(id: String, values: ContentValues): Int {
-//        return database.update(DATABASE_TABLE, values, "$_ID = ?", arrayOf(id))
-//    }
-//
-//    // delete from note where id
-//    fun delete(id: String): Int {
-//        return database.delete(DATABASE_TABLE, "$_ID = '$id'", null)
-//    }
-//}
